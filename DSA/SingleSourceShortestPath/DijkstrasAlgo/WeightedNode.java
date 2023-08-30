76766d6fe65ce670d2516549c5749f8ac5d45e8a
harshitsahu <sahuharshit.408@gmail.com>
2023-08-30 16:12:09 +0530

    Dijkstras Algorithm

---
+++ DSA/SingleSourceShortestPath/DijkstrasAlgo/WeightedNode.java
@@ -0,0 +1,30 @@
+package DSA.SingleSourceShortestPath.DijkstrasAlgo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WeightedNode implements Comparable<WeightedNode> {
+    public String name;
+    public ArrayList<WeightedNode> neighbors = new ArrayList<WeightedNode>();
+    public HashMap<WeightedNode , Integer> weightMap = new HashMap<>();
+    public boolean isVisited = false;
+    public WeightedNode parent;
+    public int index ;
+    public int distance;
+
+    public WeightedNode(String name , int index){
+        this.name = name;
+        distance = Integer.MAX_VALUE;
+        this.index = index;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    @Override
+    public int compareTo(WeightedNode o) {
+        return this.distance;
+    }
+}
