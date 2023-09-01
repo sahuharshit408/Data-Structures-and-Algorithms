540b7cacf7cc6327ff9b777fbe9eeb48820bfe80
harshitsahu <sahuharshit.408@gmail.com>
2023-09-01 09:33:32 +0530

    Bellman Ford Algorithm

---
+++ DSA/SingleSourceShortestPath/BellmanFordAlgo/Main.java
@@ -0,0 +1,28 @@
+package DSA.SingleSourceShortestPath.BellmanFordAlgo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<WeightedNode> nodeList = new ArrayList<>();
+        nodeList.add(new WeightedNode("A" , 0));
+        nodeList.add(new WeightedNode("B" , 1));
+        nodeList.add(new WeightedNode("C" , 2));
+        nodeList.add(new WeightedNode("D" , 3));
+        nodeList.add(new WeightedNode("E" , 4));
+        nodeList.add(new WeightedNode("F" , 5));
+        nodeList.add(new WeightedNode("G" , 6));
+        WeightedGraph graph = new WeightedGraph(nodeList);
+        graph.addWeightedEdge(0 , 1, 2);
+        graph.addWeightedEdge(0 , 2, 5);
+        graph.addWeightedEdge(1 , 2, 6);
+        graph.addWeightedEdge(1 , 3, 1);
+        graph.addWeightedEdge(1 , 4, 3);
+        graph.addWeightedEdge(2 , 5, 8);
+        graph.addWeightedEdge(3 , 4, 4);
+        graph.addWeightedEdge(4 , 6, 9);
+        graph.addWeightedEdge(5 , 6, 7);
+        System.out.println("Printing Bellman Ford from Source: A");
+        graph.bellmanFord(nodeList.get(0));
+    }
+}
